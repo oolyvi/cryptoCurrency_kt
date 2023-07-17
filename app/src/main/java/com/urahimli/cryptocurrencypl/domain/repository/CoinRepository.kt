963f65ee5767552d2b6e4d6013cf7b4005430d72
Ulvi <102907551+oolyvi@users.noreply.github.com>
2023-07-17 11:11:39 +0400

    Update CoinRepository.kt

--- app/src/main/java/com/urahimli/cryptocurrencypl/domain/repository/CoinRepository.kt
+++ app/src/main/java/com/urahimli/cryptocurrencypl/domain/repository/CoinRepository.kt
@@ -3,8 +3,6 @@ package com.urahimli.cryptocurrencypl.domain.repository
 import com.urahimli.cryptocurrencypl.data.remote.dto.CoinDetailDto
 import com.urahimli.cryptocurrencypl.data.remote.dto.CoinDto
 
-//test sohbetlerinde istifade ucun diger repository'den istifade olunur
-//bu fake kimidir, cox da onem dasimir
 interface CoinRepository {
 
     suspend fun getCoins(): List<CoinDto>
