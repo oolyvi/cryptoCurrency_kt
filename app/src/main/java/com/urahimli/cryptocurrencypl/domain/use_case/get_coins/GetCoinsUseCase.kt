d9d8d4d004947db0ac02c3825208b38371527a80
Ulvi <102907551+oolyvi@users.noreply.github.com>
2023-07-17 11:12:23 +0400

    Update GetCoinsUseCase.kt

--- app/src/main/java/com/urahimli/cryptocurrencypl/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/urahimli/cryptocurrencypl/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -20,7 +20,7 @@ class GetCoinsUseCase @Inject constructor(
     operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
         try {
             emit(Resource.Loading<List<Coin>>())
-            val coins = repository.getCoins().map { it.toCoin() }  //list'i coin'e cevirdik
+            val coins = repository.getCoins().map { it.toCoin() }  
             emit(Resource.Success<List<Coin>>(coins))
         } catch (e: HttpException) {
             emit(Resource.Error<List<Coin>>(e.localizedMessage ?: "An unexpected error occured!"))
